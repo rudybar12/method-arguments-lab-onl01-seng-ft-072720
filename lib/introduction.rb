024a2dc1b8c6c24c29d3259895a0f31ff0a44177
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-27 19:14:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, lanuage)
-  puts "Hi, my name is #{name} and I am learning to program #{lanuage}"
+  puts "Hi, my name is #{name} and I am learning to program in #{lanuage}"
 end
